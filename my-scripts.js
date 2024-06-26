4b94fb8b9229d2c730c847556e63ed21185d8157
JosephDuvvani <josephduvvani@gmail.com>
2024-06-26 13:20:01 +0200

    Write a function to play a One Round Game

--- my-scripts.js
+++ my-scripts.js
@@ -3,10 +3,13 @@ function getComputerChoice() {
 
     switch (randomNum) {
         case 1:
+            /*console.log('Computer Plays: rock');*/
             return 'Rock';
         case 2:
+            /*console.log('Computer Plays: Paper');*/
             return 'Paper';
         case 3:
+            /*console.log('Computer Plays: Scissors');*/
             return 'Scissors';
     }
 }
@@ -19,8 +22,42 @@ function getHumanChoice() {
         choiceCase === 'paper' ||
         choiceCase === 'Scissors'
     ) {
+        /*console.log(`Human Plays: ${choiceCase}`);*/
         return choiceCase;
     } else {
         getHumanChoice();
     }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock' && computerChoice === 'Paper') {
+        computerScore++;
+        console.log('You lose! Paper beats Rock');
+    } else if (humanChoice === 'paper' && computerChoice === 'Scissors') {
+        computerScore++;
+        console.log('You lose! Scissors beats Paper');
+    } else if (humanChoice === 'scissors' && computerChoice === 'Rock') {
+        computerScore++;
+        console.log('You lose! Rock beats Scissors');
+    } else if (humanChoice === 'paper' && computerChoice === 'Rock') {
+        humanScore++;
+        console.log('You Win! Paper beats Rock');
+    } else if (humanChoice === 'rock' && computerChoice === 'Scissors') {
+        humanScore++;
+        console.log('You Win! Rock beats Scissors');
+    } else if (humanChoice === 'scissors' && computerChoice === 'Paper') {
+        humanScore++;
+        console.log('You Win! Scissors beats Paper');
+    } else {
+        console.log('It\'s A Tie!')
+    }
+
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
