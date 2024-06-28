32b424c7ac46c9a2adf7fde5a50f78eb869187ec
JosephDuvvani <josephduvvani@gmail.com>
2024-06-28 16:10:16 +0200

    Display game data during play
    Display the running score, winner, and stop play when game is over

--- my-scripts.js
+++ my-scripts.js
@@ -3,13 +3,10 @@ function getComputerChoice() {
 
     switch (randomNum) {
         case 1:
-            console.log('Computer Plays: rock');
             return 'Rock';
         case 2:
-            console.log('Computer Plays: Paper');
             return 'Paper';
         case 3:
-            console.log('Computer Plays: Scissors');
             return 'Scissors';
     }
 }
@@ -17,47 +14,85 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === 'rock' && computerChoice === 'Paper') {
         computerScore++;
-        console.log('You lose! Paper beats Rock');
+        scoreUpdate();
+        if (gameOver()) state.textContent = `GAME OVER,  ${winner()}`;
+        results.textContent = 'You lose! Paper beats Rock';
     } else if (humanChoice === 'paper' && computerChoice === 'Scissors') {
         computerScore++;
-        console.log('You lose! Scissors beats Paper');
+        scoreUpdate();
+        if (gameOver()) state.textContent = `GAME OVER,  ${winner()}`;
+        results.textContent = 'You lose! Scissors beats Paper';
     } else if (humanChoice === 'scissors' && computerChoice === 'Rock') {
         computerScore++;
-        console.log('You lose! Rock beats Scissors');
+        scoreUpdate();
+        if (gameOver()) state.textContent = `GAME OVER,  ${winner()}`;
+        results.textContent = 'You lose! Rock beats Scissors';
     } else if (humanChoice === 'paper' && computerChoice === 'Rock') {
         humanScore++;
-        console.log('You Win! Paper beats Rock');
+        scoreUpdate();
+        if (gameOver()) state.textContent = `GAME OVER,  ${winner()}`;
+        results.textContent = 'You Win! Paper beats Rock';
     } else if (humanChoice === 'rock' && computerChoice === 'Scissors') {
         humanScore++;
-        console.log('You Win! Rock beats Scissors');
+        scoreUpdate();
+        if (gameOver()) state.textContent = `GAME OVER,  ${winner()}`;
+        results.textContent = 'You Win! Rock beats Scissors';
     } else if (humanChoice === 'scissors' && computerChoice === 'Paper') {
         humanScore++;
-        console.log('You Win! Scissors beats Paper');
+        scoreUpdate();
+        if (gameOver()) state.textContent = `GAME OVER,  ${winner()}`;
+        results.textContent = 'You Win! Scissors beats Paper';
     } else {
-        console.log('It\'s A Tie!')
+        scoreUpdate();
+        results.textContent = 'It\'s A Tie!';
     }
 
 }
 
 const btn = document.querySelector('div');
+const results = document.querySelector('.result');
+const state = document.querySelector('.game-state');
 
 btn.addEventListener('click', (e) => {
     let targ = e.target;
 
-    switch (targ.id) {
-        case 'rock-btn':
-            playRound('rock', getComputerChoice());
-            break;
-        case 'paper-btn':
-            playRound('paper', getComputerChoice());
-            break;
-        case 'scissors-btn':
-            playRound('scissors', getComputerChoice());
-            break;
+    if (gameOver() !== true) {
+        switch (targ.id) {
+            case 'rock-btn':
+                playRound('rock', getComputerChoice());
+                break;
+            case 'paper-btn':
+                playRound('paper', getComputerChoice());
+                break;
+            case 'scissors-btn':
+                playRound('scissors', getComputerChoice());
+                break;
+        }
     }
 });
 
-
-
 let humanScore = 0;
 let computerScore = 0;
+
+function gameOver() {
+    if (humanScore === 5 || computerScore === 5) {
+        return true;
+    } else return false;
+
+}
+
+const humanScoreBoard = document.querySelector('#human-score');
+const compScoreBoard = document.querySelector('#comp-score');
+
+function scoreUpdate() {
+    humanScoreBoard.textContent = `Human: ${humanScore}`;
+    compScoreBoard.textContent = `Computer: ${computerScore}`;
+}
+
+function winner() {
+    if (humanScore > computerScore) {
+        return 'WINNER: Human!!!';
+    } else if (computerScore > humanScore) {
+        return 'WINNER: Computer!!!'
+    }
+}
